ee80e0dd6a69e0e6e325364f1ca37220ca5a3d81
Patryk Milczarczyk <housben20@gmail.com>
2025-08-31 12:18:13 +0200

    fast fix

--- src/app/(dashboard)/components/PortfolioDashboard.tsx
+++ src/app/(dashboard)/components/PortfolioDashboard.tsx
@@ -27,6 +27,7 @@ export default function PortfolioDashboard(){
     const [mainCurrency,setMainCurrency] = useState("");
 
     const baseURL = process.env.PUBLIC_BASE_URL || "http://localhost:3000";
+    console.log(baseURL);
 
     const getAssets = async () =>{
         setIsLoading(true);
